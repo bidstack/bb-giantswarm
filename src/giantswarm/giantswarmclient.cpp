057cdefdba3599b65fc179ff13ecd9ebeb2a7cf3
Jan Pieper <kontakt@jan-pieper.info>
2015-01-29 21:46:06 +0100

    added giantswarm api client

---
+++ src/giantswarm/giantswarmclient.cpp
@@ -0,0 +1,134 @@
+#include "giantswarmclient.hpp"
+
+GiantswarmClient::GiantswarmClient(QObject *parent) : QObject(parent) {
+
+}
+
+/**
+ * Authentication
+ */
+
+bool GiantswarmClient::login(QString email, QString password) {
+    Q_UNUSED(email);
+    Q_UNUSED(password);
+    return false;
+}
+
+bool GiantswarmClient::logout() {
+    return false;
+}
+
+bool GiantswarmClient::isLoggedIn() {
+    return false;
+}
+
+/**
+ * Companies
+ */
+
+QVariantList GiantswarmClient::getCompanies() {
+    QVariantList companies;
+    return companies;
+}
+
+bool GiantswarmClient::hasCompanies() {
+    return getCompanies().size() > 0;
+}
+
+bool GiantswarmClient::createCompany(QString companyName) {
+    Q_UNUSED(companyName);
+    return false;
+}
+
+bool GiantswarmClient::deleteCompany(QString companyName) {
+    Q_UNUSED(companyName);
+    return false;
+}
+
+/**
+ * Company users
+ */
+
+QVariantList GiantswarmClient::getCompanyUsers(QString companyName) {
+    Q_UNUSED(companyName);
+    QVariantList users;
+    return users;
+}
+
+bool GiantswarmClient::addUserToCompany(QString companyName, QString username) {
+    Q_UNUSED(companyName);
+    Q_UNUSED(username);
+    return false;
+}
+
+bool GiantswarmClient::removeUserFromCompany(QString companyName, QString username) {
+    Q_UNUSED(companyName);
+    Q_UNUSED(username);
+    return false;
+}
+
+/**
+ * Environments
+ */
+
+QVariantList GiantswarmClient::getEnvironments() {
+    QVariantList environments;
+    return environments;
+}
+
+bool GiantswarmClient::hasEnvironments() {
+    return false;
+}
+
+bool GiantswarmClient::hasEnvironment(QString companyName, QString environmentName) {
+    Q_UNUSED(companyName);
+    Q_UNUSED(environmentName);
+    return false;
+}
+
+bool GiantswarmClient::createEnvironment(QString companyName, QString environmentName) {
+    Q_UNUSED(companyName);
+    Q_UNUSED(environmentName);
+    return false;
+}
+
+bool GiantswarmClient::deleteEnvironment(QString companyName, QString environmentName) {
+    Q_UNUSED(companyName);
+    Q_UNUSED(environmentName);
+    return false;
+}
+
+/**
+ * Applications
+ */
+
+QVariantMap GiantswarmClient::getApplications() {
+    QVariantMap applications;
+    return applications;
+}
+
+/**
+ * Instances
+ */
+
+QVariantMap GiantswarmClient::getInstanceStatistics(QString companyName, QString instanceId) {
+    Q_UNUSED(companyName);
+    Q_UNUSED(instanceId);
+    QVariantMap statistics;
+    return statistics;
+}
+
+/**
+ * Account
+ */
+
+bool GiantswarmClient::updateEmail(QString email) {
+    Q_UNUSED(email);
+    return false;
+}
+
+bool GiantswarmClient::updatePassword(QString password) {
+    Q_UNUSED(password);
+    return false;
+}
+
