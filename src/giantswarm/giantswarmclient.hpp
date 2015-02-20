402e40dc316e31c0867a8b4b2cea4d7c736d447c
Jan Pieper <kontakt@jan-pieper.info>
2015-02-20 19:02:26 +0100

    added method to fetch application status

--- src/giantswarm/giantswarmclient.hpp
+++ src/giantswarm/giantswarmclient.hpp
@@ -45,6 +45,7 @@ namespace Giantswarm {
         Q_INVOKABLE bool deleteEnvironment(QString companyName, QString environmentName);
 
         Q_INVOKABLE QVariantMap getApplications();
+        Q_INVOKABLE QVariantMap getApplicationStatus(QString companyName, QString environmentName, QString applicationName);
         Q_INVOKABLE QVariantMap getApplicationConfiguration(QString companyName, QString environmentName, QString applicationName);
         Q_INVOKABLE bool startApplication(QString companyName, QString environmentName, QString applicationName);
         Q_INVOKABLE bool stopApplication(QString companyName, QString environmentName, QString applicationName);
